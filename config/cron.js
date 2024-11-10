7a2c45fc1edaa033fc06a97dbff3129ea60ab2c0
Eduardo Muramoto <dudmuramoto@gmail.com>
2024-11-10 12:50:46 +1100

    time

--- config/cron.js
+++ config/cron.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*/10 * * * *': async () => {
+  '*/5 * * * *': async () => {
     const currentDate = new Date();
     console.log(`Current Date and Time: ${currentDate}`);
   },
